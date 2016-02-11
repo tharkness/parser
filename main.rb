682cc6abb5392e8a9213501929a9b3f771849d86
vagrant <vagrant@lighthouse-labs>
2016-02-11 03:05:29 +0000

    fixed item_id and url

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require_relative 'lib/post'
 require 'nokogiri'
+require 'pry'
 
 html_file = open(ARGV[0])
 post = html_file.read
@@ -12,36 +13,14 @@ def create_post(doc)
   title = doc.search('td.title a')[0].inner_text
   points = doc.search('td.subtext span')[0].inner_text
   id = get_id(doc)
-  url = doc.search('td.title a')[1].inner_text
+  url = doc.xpath("//td[@class='title']/a/@href").first.value
   return Post.new(title, url, points, id)
 end
 
 def get_id(doc)
-  doc.search('td.subtext a')[1].attributes.values.map do |i|
-    /\d+/.match(i.to_s)[0]
-  end
+  first = doc.xpath("//td[@class='subtext']/span[@class='age']/a/@href").first.value
+  first[/\d+/]
 end
-
-# def comment_loop
-#   html_file = open('post.html')
-#   post = html_file.read
-#   doc = Nokogiri::HTML(post)
-#   i = 1
-#   x = 0
-#   doc.search('.comhead').length do |i, x|
-#     comm = Comment.new(doc, i, x)
-#     something.add_comment(comm)
-#     i += 1
-#     x += 1
-#   end
-# end
-
-# def create_comment
-#   user = doc.search('.comhead > a')[user_idx].inner_text
-#   time = doc.search('.comhead > .age')[idx].inner_text
-#   text = doc.search('.comment')[idx].inner_text
-#   return Comment.new()
-# end
 
 posty = create_post(doc)
 
